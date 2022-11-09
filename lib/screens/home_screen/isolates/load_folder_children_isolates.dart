83c2a0d3f8fdb7c56fde71beea6ea1ee3d7fe3fa
Amr M Hassan <71602963+amrmhassan@users.noreply.github.com>
2022-11-09 16:40:59 +0200

    before making listing a dir children an isolate

--- lib/screens/home_screen/isolates/load_folder_children_isolates.dart
+++ lib/screens/home_screen/isolates/load_folder_children_isolates.dart
@@ -1,31 +1,50 @@
+import 'dart:async';
 import 'dart:io';
+import 'dart:isolate';
 
-import 'package:explorer/utils/general_utils.dart';
+import 'package:explorer/models/storage_item_model.dart';
+import 'package:explorer/models/types.dart';
 
-Future<List<FileSystemEntityInfo>> getFolderChildrenIsolate(String path) async {
-  if (path.contains('Book')) {
-    printOnDebug('Books');
-  }
-  Directory directory = Directory(path);
-  var data = directory.listSync();
-  List<FileSystemEntityInfo> childrenInfo = [];
-  for (var entity in data) {
-    int fSize = (await entity.stat()).size;
-    childrenInfo.add(FileSystemEntityInfo(
-      fileSystemEntity: entity,
-      size: fSize,
-    ));
-  }
+//? this should run in the home screen init state
+void loadExplorerChildren(SendPort sendPort) {
+  ReceivePort receivePort = ReceivePort();
+  StreamSubscription<FileSystemEntity>? streamSub;
+  sendPort.send(receivePort.sendPort);
+  //? this will hold maximum 10 children
+  List<StorageItemModel> cachedChildren = [];
 
-  return childrenInfo;
-}
-
-class FileSystemEntityInfo {
-  final FileSystemEntity fileSystemEntity;
-  final int size;
-
-  const FileSystemEntityInfo({
-    required this.fileSystemEntity,
-    required this.size,
+  receivePort.listen((message) {
+    if (streamSub != null) {
+      streamSub!.cancel();
+    }
+    if (message is String) {
+      //? this is a path
+      Directory directory = Directory(message);
+      var stream = directory.list();
+      streamSub = stream.listen((entity) async {
+        if (cachedChildren.length > 10) {
+          //? here send the list then clear it
+          sendPort.send(cachedChildren);
+          cachedChildren.clear();
+        } else {
+          //? here just add to the list
+          FileStat entityStat = await entity.stat();
+          StorageItemModel storageItemModel = StorageItemModel(
+            parentPath: entity.parent.path,
+            path: entity.path,
+            modified: entityStat.modified,
+            accessed: entityStat.accessed,
+            changed: entityStat.changed,
+            entityType: entityStat.type == FileSystemEntityType.directory
+                ? EntityType.folder
+                : EntityType.file,
+            size: entityStat.type == FileSystemEntityType.directory
+                ? null
+                : entityStat.size,
+          );
+          cachedChildren.add(storageItemModel);
+        }
+      });
+    }
   });
 }
