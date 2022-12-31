36562626392c650e130ebb2338e8fe9ad1a47735
Amr M Hassan <71602963+amrmhassan@users.noreply.github.com>
2022-12-31 17:07:35 +0200

    some improvments in kotlin code made a separate class for creating a different class for creating files tumbnails

--- android/app/src/main/kotlin/com/amh/file_manager/MainActivity.kt
+++ android/app/src/main/kotlin/com/amh/file_manager/MainActivity.kt
@@ -1,17 +1,10 @@
 package com.amh.file_manager
 
-import android.graphics.Bitmap
-import android.graphics.BitmapFactory
-import android.graphics.Canvas
-import android.graphics.Color
-import android.graphics.drawable.Drawable
-import android.media.MediaMetadataRetriever
 import androidx.annotation.NonNull
+import com.amh.file_manager.helpers.ThumbnailCreator
 import io.flutter.embedding.android.FlutterActivity
 import io.flutter.embedding.engine.FlutterEngine
 import io.flutter.plugin.common.MethodChannel
-import java.io.File
-import java.io.FileOutputStream
 import kotlin.concurrent.thread
 
 class MainActivity : FlutterActivity() {
@@ -23,112 +16,36 @@ class MainActivity : FlutterActivity() {
             "amh.fileManager.com/create_thumbnail"
         )
         .setMethodCallHandler { call, result ->
+          val filePath = call.argument<String>("filePath")!!
+          val outputPath = call.argument<String>("output")!!
+          val thumbnailWidth = call.argument<Int>("width")!!
+          val tc = ThumbnailCreator(filePath, outputPath, thumbnailWidth)
           thread {
-            print("message")
             try {
               if (call.method == "handleVideo") {
 
-                val filePath = call.argument<String>("filePath")!!
                 val time = call.argument<Long>("time")!!
-                val outputPath = call.argument<String>("output")!!
-                val thumbnailWidth = call.argument<Int>("width")!!
-                val thumbnail = createVideoThumbnail(filePath, time)!!
-                saveBitmapToFile(thumbnail, File(outputPath), thumbnailWidth)
-                result.success(outputPath)
-              } else if (call.method == "handleAPK") {
+                val thumbnailPath = tc.createVideoThumbnail(filePath, time)!!
 
-                val filePath = call.argument<String>("filePath")!!
-                val outputPath = call.argument<String>("output")!!
-                val thumbnailWidth = call.argument<Int>("width")!!
-                val thumbnail = makeAPKIcon(filePath, outputPath, thumbnailWidth)
-                result.success(thumbnail)
+                result.success(thumbnailPath)
+              } else if (call.method == "handleAPK") {
+                val pm = getPackageManager()
+
+                val thumbnailPath = tc.createAPKThumnail(filePath, outputPath, pm)
+
+                result.success(thumbnailPath)
               } else if (call.method == "handleImage") {
 
-                val filePath = call.argument<String>("filePath")!!
-                val outputPath = call.argument<String>("output")!!
-                val thumbnailWidth = call.argument<Int>("width")!!
-
-                makeImageThumbnail(filePath, outputPath, thumbnailWidth)
-                result.success(outputPath)
+                val thumbnailPath = tc.createImageThumbnail(filePath)
+                result.success(thumbnailPath)
               } else {
                 result.notImplemented()
               }
             } catch (e: Exception) {
+              result.success(e.toString())
               print("An error occured creating a thumbnail")
             }
           }
         }
-  }
-
-  // ? to create a video thumbnail
-  fun createVideoThumbnail(videoPath: String, time: Long): Bitmap? {
-    val retriever = MediaMetadataRetriever()
-    try {
-      retriever.setDataSource(videoPath)
-      return retriever.getFrameAtTime(time)
-    } catch (e: Exception) {
-      e.printStackTrace()
-      return null
-    } finally {
-      retriever.release()
-    }
-  }
-
-  // ? to save a bitmap to a file
-  fun saveBitmapToFile(bitmap: Bitmap, file: File, newWidth: Int) {
-    val out = FileOutputStream(file)
-    try {
-
-      val originalWidth = bitmap.width
-      val originalHeight = bitmap.height
-      val compressFactor = (originalWidth * 1.0) / (newWidth * 1.0)
-      val newHeight = originalHeight / compressFactor
-      val newWidthCalced = originalWidth / compressFactor
-
-      val scaledBitmap =
-          Bitmap.createScaledBitmap(bitmap, newWidthCalced.toInt(), newHeight.toInt(), false)
-      scaledBitmap.compress(Bitmap.CompressFormat.PNG, 15, out)
-    } finally {
-      out.close()
-    }
-  }
-
-  // ? to make an apk icon
-  fun makeAPKIcon(apkFilePath: String, outputFile: String, width: Int): String {
-    val pm = getPackageManager()
-    val pi = pm.getPackageArchiveInfo(apkFilePath, 0)
-
-    // to set the object to the wanted apk file
-    pi!!.applicationInfo.sourceDir = apkFilePath
-    pi.applicationInfo.publicSourceDir = apkFilePath
-    //
-
-    val apkicon = pi.applicationInfo.loadIcon(pm)!!
-    // val appName = pi.applicationInfo.loadLabel(pm)
-    val thumbPath = drawableToBitmap(apkicon, outputFile, width)
-    return thumbPath
-  }
-
-  // ? to convert drawble to bitmap (needed in the apk thumbnails)
-  fun drawableToBitmap(drawable: Drawable, outputFile: String, width: Int): String {
-    val bitmap =
-        Bitmap.createBitmap(
-            drawable.intrinsicWidth,
-            drawable.intrinsicHeight,
-            Bitmap.Config.ARGB_8888
-        )
-    bitmap.eraseColor(Color.TRANSPARENT)
-    val canvas = Canvas(bitmap)
-    drawable.setBounds(0, 0, canvas.width, canvas.height)
-    drawable.draw(canvas)
-    val file = File(outputFile)
-    saveBitmapToFile(bitmap, file, width)
-
-    return file.path
-  }
-
-  // ? to  make an image thumbnail
-  fun makeImageThumbnail(imagePath: String, outputFile: String, width: Int) {
-    saveBitmapToFile(BitmapFactory.decodeFile(imagePath), File(outputFile), width)
   }
 }
---
+++ android/app/src/main/kotlin/com/amh/file_manager/helpers/ThumbnailsCreator.kt
@@ -0,0 +1,90 @@
+package com.amh.file_manager.helpers
+
+import android.content.pm.PackageManager
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
+import android.graphics.Canvas
+import android.graphics.Color
+import android.graphics.drawable.Drawable
+import android.media.MediaMetadataRetriever
+import io.flutter.embedding.android.FlutterActivity
+import java.io.File
+import java.io.FileOutputStream
+
+class ThumbnailCreator(var filePath: String, var outputPath: String, var thumbnailWidth: Int) :
+    FlutterActivity() {
+
+  // ? to  make an image thumbnail
+  fun createImageThumbnail(imagePath: String): String {
+    this.saveBitmapToFile(BitmapFactory.decodeFile(imagePath), File(outputPath), thumbnailWidth)
+    return outputPath
+  }
+  // ? to create a video thumbnail
+  fun createVideoThumbnail(videoPath: String, time: Long): String? {
+    val retriever = MediaMetadataRetriever()
+    try {
+      retriever.setDataSource(videoPath)
+      val bitmap = retriever.getFrameAtTime(time)!!
+      this.saveBitmapToFile(bitmap, File(outputPath), thumbnailWidth)
+      return outputPath
+    } catch (e: Exception) {
+      e.printStackTrace()
+      return null
+    } finally {
+      retriever.release()
+    }
+  }
+
+  // ? to make an apk icon
+  fun createAPKThumnail(apkFilePath: String, outputFile: String, pm: PackageManager): String {
+
+    val pi = pm.getPackageArchiveInfo(apkFilePath, 0)
+
+    // to set the object to the wanted apk file
+    pi!!.applicationInfo.sourceDir = apkFilePath
+    pi.applicationInfo.publicSourceDir = apkFilePath
+    //
+
+    val apkicon = pi.applicationInfo.loadIcon(pm)!!
+    // val appName = pi.applicationInfo.loadLabel(pm)
+    val thumbPath = this.drawableToBitmap(apkicon, outputFile, thumbnailWidth)
+    return thumbPath
+  }
+
+  // ? to save a bitmap to a file
+  private fun saveBitmapToFile(bitmap: Bitmap, file: File, newWidth: Int) {
+    val out = FileOutputStream(file)
+    try {
+
+      val originalWidth = bitmap.width
+      val originalHeight = bitmap.height
+      val compressFactor = (originalWidth * 1.0) / (newWidth * 1.0)
+      val newHeight = originalHeight / compressFactor
+      val newWidthCalced = originalWidth / compressFactor
+
+      val scaledBitmap =
+          Bitmap.createScaledBitmap(bitmap, newWidthCalced.toInt(), newHeight.toInt(), false)
+      scaledBitmap.compress(Bitmap.CompressFormat.PNG, 15, out)
+    } finally {
+      out.close()
+    }
+  }
+
+  // ? to convert drawble to bitmap (needed in the apk thumbnails)
+  private fun drawableToBitmap(drawable: Drawable, outputFile: String, width: Int): String {
+    val bitmap =
+        Bitmap.createBitmap(
+            drawable.intrinsicWidth,
+            drawable.intrinsicHeight,
+            Bitmap.Config.ARGB_8888
+        )
+    bitmap.eraseColor(Color.TRANSPARENT)
+    val canvas = Canvas(bitmap)
+    drawable.setBounds(0, 0, canvas.width, canvas.height)
+    drawable.draw(canvas)
+    val file = File(outputFile)
+    this.saveBitmapToFile(bitmap, file, width)
+
+    return file.path
+  }
+}
