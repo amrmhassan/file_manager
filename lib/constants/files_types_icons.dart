d49e7ad7c1ffb2080bf8251269e1fef311a15828
Amr M Hassan <71602963+amrmhassan@users.noreply.github.com>
2023-01-04 12:23:47 +0200

    more files extensions added

--- lib/constants/files_types_icons.dart
+++ lib/constants/files_types_icons.dart
@@ -7,6 +7,10 @@ enum FileType {
   docs,
   unknown,
   apk,
+  word,
+  excel,
+  powerPoint,
+  text,
 }
 
 List<String> docsExt = [
@@ -140,8 +144,43 @@ List<String> archivesExt = [
   'iso',
 ];
 
+List<String> excelsExt = [
+  'xls',
+  'xlsx',
+  'xlsm',
+  'xltx',
+  'xltm',
+  'xlsb',
+  'csv',
+];
+List<String> wordExt = [
+  'doc',
+  'docx',
+  'docm',
+  'dotx',
+  'dotm',
+];
+List<String> powerPointExt = [
+  'ppt',
+  'pptx',
+  'pptm',
+  'potx',
+  'potm',
+];
+List<String> textExt = [
+  'txt',
+  'log',
+  'conf',
+  'asc',
+];
+List<String> richTextExt = [
+  'rtf',
+];
+
+//? this if to get the file type in general and have no relationship with file icon
 FileType getFileType(String extension) {
   String ext = extension.toLowerCase().replaceAll('.', '');
+
   if (audioExt.contains(ext)) {
     return FileType.audio;
   } else if (videoExt.contains(ext)) {
@@ -161,12 +200,25 @@ FileType getFileType(String extension) {
   }
 }
 
+//? this is only to get file icon path
 String getFileTypeIcon(String extension) {
+  // here you need to check the specific file extensions first before the general ones,
+  // for example check the word, excel, text extensions before the docs ones
   String ext = extension.toLowerCase().replaceAll('.', '');
   String iconPackFolder = 'assets/ext_icons/icons_1/';
   String extImage = 'unknown';
   //* checking audio
-  if (audioExt.contains(ext)) {
+  if (excelsExt.contains(ext)) {
+    extImage = 'xls';
+  } else if (wordExt.contains(ext)) {
+    extImage = 'doc';
+  } else if (powerPointExt.contains(ext)) {
+    extImage = 'pptx';
+  } else if (textExt.contains(ext)) {
+    extImage = 'text';
+  } else if (richTextExt.contains(ext)) {
+    extImage = 'text-rtf';
+  } else if (audioExt.contains(ext)) {
     extImage = 'audio';
   } else if (videoExt.contains(ext)) {
     extImage = 'video';
